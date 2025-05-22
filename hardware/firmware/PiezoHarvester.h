46a3146e8312f627ba2ffe63ff4c04459f5ab798
Bryce Wooster <40742705+BryceWDesign@users.noreply.github.com>
2025-05-22 15:52:27 -0700

    Add PiezoHarvester class definition for analog energy harvesting estimation

--- hardware/firmware/PiezoHarvester.h
+++ hardware/firmware/PiezoHarvester.h
@@ -1,22 +1,23 @@
 // PiezoHarvester.h
-// IX-DroneShowcase: Header for Piezoelectric Energy Harvester Class
+// IX-DroneShowcase: Header for Piezoelectric Energy Harvester
 // Author: BryceWDesign
 // License: MIT
 
 #ifndef PIEZOHARVESTER_H
 #define PIEZOHARVESTER_H
 
-#define PIEZO_INPUT_PIN A0
+#define PIEZO_INPUT_PIN A0  // ADC input for piezo signal
 
 class PiezoHarvester {
-  public:
-    void init();
-    int readEnergy();
+public:
+  void init();         // Initialize the pin and variables
+  int readEnergy();    // Read and return estimated power in mW
 
-  private:
-    int estimateEnergy(int smoothed);
-    float smoothedValue;
-    const float internalResistance = 100000.0; // ohms
+private:
+  float smoothedValue;             // For signal smoothing
+  const float internalResistance = 100000.0; // Assumed resistance in ohms
+
+  int estimateEnergy(int smoothed); // Convert signal to estimated mW
 };
 
 #endif
