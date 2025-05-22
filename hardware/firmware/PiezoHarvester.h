61f1b6dbedb6aef0973ad1a3e8c79e328345be08
Bryce Wooster <40742705+BryceWDesign@users.noreply.github.com>
2025-05-22 15:59:29 -0700

    Add PiezoHarvester class definition and constants

--- hardware/firmware/PiezoHarvester.h
+++ hardware/firmware/PiezoHarvester.h
@@ -6,16 +6,16 @@
 #ifndef PIEZO_HARVESTER_H
 #define PIEZO_HARVESTER_H
 
-#define PIEZO_INPUT_PIN A0  // Analog pin for piezo input
+#define PIEZO_INPUT_PIN A0
 
 class PiezoHarvester {
 public:
   void init();
-  int readEnergy();  // Returns estimated power in mW
+  int readEnergy();
 
 private:
   float smoothedValue;
-  const float internalResistance = 1000.0; // ohms
+  const float internalResistance = 1000.0; // Ohms, approximate
   int estimateEnergy(int smoothed);
 };
 
